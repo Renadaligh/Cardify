40cc16380c4a76b721b37c6d77b18d3b0d494d56
renadaligh <renadaligh@outlook.com>
2026-03-18 01:28:53 +0300

    Modify: Repo name

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/OccasionCards/',
+  base: '/Cardify/',
   plugins: [react()],
 })
